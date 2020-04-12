5bae22e7dbeff5a1e26ef626acb81a4d459803a0
gwfellows <33993581+gwfellows@users.noreply.github.com>
2020-04-12 16:30:04 -0700

    clean up folder
    Removed things that should not have been there

--- hi.py
+++
@@ -1,8 +0,0 @@
-import pygame, sys
-pygame.init()
-screen = pygame.set_mode([640, 480])
-screen. fill([255, 255, 255])
-while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			sys.exit()
