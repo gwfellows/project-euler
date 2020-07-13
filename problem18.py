fd9786dbaeda1f04bfa61b6dcfacff6e78f10be7
grantfellows <56371699+grantfellows@users.noreply.github.com>
2020-07-12 17:38:05 -0700

    solved 18 (brute force)

--- problem18.py
+++ problem18.py
@@ -31,53 +31,51 @@ NOTE: As there are only 16384 routes, it is possible to solve this problem by tr
 However, Problem 67, is the same challenge with a triangle containing one-hundred rows; 
 it cannot be solved by brute force, and requires a clever method! ;o)
 '''
+triangleStr = '''75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23'''
 
-triangle = [[75],
-[95,64],
-[17,47,82],
-[18,35,87,10],
-[20,4,82,47,65],
-[19,1,23,75,3,34],
-[88,2,77,73,7,63,67],
-[99,65,4,28,6,16,70,92],
-[41,41,26,56,83,40,80,70,33],
-[41,48,72,33,47,32,37,16,94,29],
-[53,71,44,65,25,43,91,52,97,51,14],
-[70,11,33,28,77,73,17,78,39,68,17,57],
-[91,71,52,38,17,14,91,43,58,50,27,29,48],
-[63,66,4,68,89,53,67,30,73,16,69,87,40,31],
-[4,62,98,27,23,9,70,98,73,93,38,53,60,4,23]]
+triangleLst = triangleStr.split("\n")
 
-#print ((triangle[1])[1])
+triangle = []
+
+for i in triangleLst:
+	triangle.append(i.split(" "))
+
+for i in triangle:
+	i[:] = list(map(int, i))
+
+def top(tri):
+	return tri[0][0]
+
+def left(tri):
+	leftTri = []
+	for x in tri[1:]:
+		leftTri.append(x[:-1])
+	return(leftTri)
+
+def right(tri):
+	rightTri = []
+	for x in tri[1:]:
+		rightTri.append(x[1:])
+	return(rightTri)
+
+def maxPaths(tri):
+	if len(tri) == 0:
+		return 0
+	return top(tri) + max(maxPaths(left(tri)), maxPaths(right(tri)))
 
 
-paths = list()
-
-rowcounter = 0
-positioncounter = 0
-
-# 15 rows (0-14)
-# 1 to 15 positions (0-14)
-
-pathsumlist = list()
-pathsum = 0
-
-while rowcounter < 15:
-	pathsum = pathsum + (triangle[rowcounter])[positioncounter]
-	print ("Row:" + str(rowcounter))
-	for position in (triangle[rowcounter]):
-		print ("Row:" + str(rowcounter) + " Pos. counter:" + str(positioncounter) + " Value:" + str(position))
-		positioncounter += 1
-		
-	rowcounter += 1
-	positioncounter = 0
-
-#print (pathsum)
-
-'''
-for row in triangle:
-	print (row)
-	for position in row:
-		print (position)
-	
-'''	
+print(maxPaths(triangle))
