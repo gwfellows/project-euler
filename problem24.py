ce3ee4298925b12899c08a2ee000f063d29df571
grantfellows <56371699+grantfellows@users.noreply.github.com>
2020-07-17 11:11:13 -0700

    solved 24 (first 25 now solved!)

--- problem24.py
+++ problem24.py
@@ -7,5 +7,59 @@ The lexicographic permutations of 0, 1 and 2 are:
 
 What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 '''
+
 #notes:
-#see 
+#see https://en.wikipedia.org/wiki/Permutation#Generation_in_lexicographic_order
+
+# Find the largest index k such that a[k] < a[k + 1].
+def findK(a):
+    k = len(a) - 2
+    while k > 0:
+        if a[k] < a[k + 1]:
+            return k
+        k -= 1
+    return k
+
+# Find the largest index l greater than k such that a[k] < a[l].
+def findL(a, k):
+    l = len(a) - 1
+    while l > k:
+        if a[k] < a[l]:
+            return l
+        l -= 1
+    return l
+
+# Swap the value of a[k] with that of a[l].
+def swap(k, l):
+    global a
+    a[k], a[l] = a[l], a[k]
+
+def reverse(start, stop):
+    global a
+    size = stop + start
+    for i in range(start, (size + 1) // 2 ):
+        j = size - i
+        a[i], a[j] = a[j], a[i]
+
+# Reverse the sequence from a[k + 1] up to and including the final element a[n]
+def reverseN(k):
+    global a
+    reverse(k+1, len(a)-1)
+
+num = 0
+
+a = list(range(0,10))
+
+while num <= 1000000-2:
+    # print(a)
+    k = findK(a)
+    # print(a)
+    # print("-")
+    l = findL(a, k)
+    swap(k, l)
+    reverseN(k)
+    #print(a)
+    num += 1
+
+
+print("".join(map(str, a)))
