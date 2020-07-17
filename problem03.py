c3d79c72ffa7ff27988453f1c663b98375be4438
grantfellows <56371699+grantfellows@users.noreply.github.com>
2020-07-17 11:57:16 -0700

    make problem 3 fast

--- problem03.py
+++ problem03.py
@@ -1,50 +1,29 @@
 #The prime factors of 13195 are 5, 7, 13 and 29.
 #What is the largest prime factor of the number 600851475143 ?
 
-largestprimefactor = 0
-smallestprimefactor = 0
-limit = 600851475143
-#limit = 100
-'''
-j = 2
-while j <= limit:
-	prime = True
-	if limit % j == 0:
-		print (str(j) + " is a factor")
-		counter = 2
-		while counter < j:
-			if j % counter == 0:
-				prime = False
-				print (str(j) + " is not prime")
-				break
-			counter = counter + 1
-		if prime == True:
-			smallestprimefactor = j 		
-			print ("Smallest prime factor: " + str(smallestprimefactor))
-			break
-	j = j + 1'''
+bigNum = 600851475143
 
-#largestfactor = limit / smallestprimefactor
+import math
 
-#print ("Largest factor: " + str(largestfactor))
+def isPrime(n):
+    prime = True
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
 
-i = 6935222310
-print("go!")
-while i <= limit:
-	prime = True
-	#print ("Looking at " + str(i))
-	if limit % i == 0:
-		#print (str(i) + " is a factor")
-		counter = 2
-		while counter < i:
-			if i % counter == 0:
-				prime = False
-				#print (str(i) + " is not prime")
-				break
-			counter = counter + 1
-		if prime == True:
-			largestprimefactor = i 		
-			print ("Largest prime factor: " + str(largestprimefactor))
-			break	
-	i = i - 1
+    for i in range(2, math.floor(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return prime
+
+factor = 2
+
+n =  math.floor(math.sqrt(bigNum)) + 1
+while n > 0:
+	if bigNum % n == 0:
+		if isPrime(n):
+			print(n)
+			exit()
+	n -= 1
 
