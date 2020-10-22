652eb7a91a6186c1cb4bd670dc70747d19235f8a
grantfellows <56371699+grantfellows@users.noreply.github.com>
2020-10-21 21:45:28 -0700

    solved 27!

--- problem27.py
+++ problem27.py
@@ -14,4 +14,32 @@ where |n| is the modulus/absolute value of n /> e.g. |11| = 11 and |-4| = 4
 Find the product of the coefficients, a and b, 
 for the quadratic expression that produces the maximum number of primes for consecutive values of n, starting with n = 0
 '''
+import math
+
+def is_prime(n):
+    prime = True
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+
+    for i in range(2, math.floor(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return prime
+
+#           a*b, n_primes  
+solution = [0,   0]
+
+for a in range(-1000,1000):
+    for b in range(-1000,1000):
+        n = 0
+        while is_prime(n**2 + a*n + b):
+            n += 1
+        if n > solution[1]:
+            print(a, b)
+            solution = [a*b, n]
+
+print(solution[0])
+
 
