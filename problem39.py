7ba7ac55343d2d2c78156b556fd689806f496749
grantfellows <gwfellows@icloud.com>
2021-03-26 18:19:38 -0700

    solve 39 (a bit slow)

--- problem39.py
+++ problem39.py
@@ -1,26 +1,18 @@
-"""
-p = a + b + c
-forall
-	a ^ 2 + b ^ 2 = c ^ 2
-	a = int, b = int, c=int
-	a>0, b>0, c>0
-"""
-
 from multiprocessing import Pool
 
 
 def get_num_solutions(p):
-    solutions = set()
+    nsols = 0
     for a in range(1, p):
         for b in range(1, p - a):
-            for c in range(1, p - b):
-                if (a ** 2 + b ** 2 == c ** 2) and (a + b + c == p):
-                    solutions.add(tuple(sorted((a, b, c))))
-    return len(solutions)
+            c = p - a - b
+            if a ** 2 + b ** 2 == c ** 2:
+                nsols += 1
+    return nsols
 
 
 if __name__ == "__main__":
-    p = Pool(1000)
-    sols = p.map(get_num_solutions, range(1, 1000))
+    p = Pool(50)
+    sols = p.map(get_num_solutions, range(1, 1000 + 1))
     largest = max(sols)
     print(sols.index(largest) + 1)
