438c8cb99c2ac18e0715543f618e2cb95ed2d7ea
grantfellows <gwfellows@icloud.com>
2021-03-28 19:00:42 -0700

    solve 43

---
+++ problem43.py
@@ -0,0 +1,43 @@
+"""
+The number, 1406357289, is a 0 to 9 pandigital number because it is made up of each of the digits 0 to 9 in some order,
+but it also has a rather interesting sub-string divisibility property.
+
+Let d1 be the 1st digit, d2 be the 2nd digit, and so on. In this way, we note the following:
+
+	d2d3d4=406 is divisible by 2
+	d3d4d5=063 is divisible by 3
+	d4d5d6=635 is divisible by 5
+	d5d6d7=357 is divisible by 7
+	d6d7d8=572 is divisible by 11
+	d7d8d9=728 is divisible by 13
+	d8d9d10=289 is divisible by 17
+
+Find the sum of all 0 to 9 pandigital numbers with this property.
+"""
+
+
+def is_interesting(num):
+    def substr_div(n, a, b, c, d):
+        return int(n[a - 1] + n[b - 1] + n[c - 1]) % d == 0
+
+    return (
+        substr_div(num, 2, 3, 4, 2)
+        and substr_div(num, 3, 4, 5, 3)
+        and substr_div(num, 4, 5, 6, 5)
+        and substr_div(num, 5, 6, 7, 7)
+        and substr_div(num, 6, 7, 8, 11)
+        and substr_div(num, 7, 8, 9, 13)
+        and substr_div(num, 8, 9, 10, 17)
+    )
+
+
+from itertools import permutations
+
+ans = 0
+
+for num in permutations("0123456789"):
+    num = "".join(num)
+    if is_interesting(num):
+        ans += int(num)
+
+print(ans)
