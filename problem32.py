323452c3aebc0474f4e8b1ff4a1da4686c60c260
grantfellows <gwfellows@icloud.com>
2021-03-30 09:49:35 -0700

    shorten 32

--- problem32.py
+++ problem32.py
@@ -6,10 +6,8 @@ for perm in permutations("123456789"):
     base = "".join(perm)
     for splita in range(1, 2 + 1):
         splitb = (9 - splita) // 2 + splita
-        a = base[:splita]
-        b = base[splita:splitb]
-        c = base[splitb:]
-        if int(a) * int(b) == int(c):
-            products.add(int(c))
+        a, b, c = int(base[:splita]), int(base[splita:splitb]), int(base[splitb:])
+        if a * b == c:
+            products.add(c)
 
 print(sum(products))
