857dbc98022d576f39333e188b75d8b98b74d576
grantfellows <gwfellows@icloud.com>
2021-09-26 20:32:11 -0700

    speed up 57

--- problem57.py
+++ problem57.py
@@ -1,23 +1,8 @@
-from fractions import Fraction
-from functools import cache
-from sys import setrecursionlimit
-
-setrecursionlimit(2500)
-
-
-def expansion(iters):
-    @cache
-    def term(i, iters):
-        return Fraction(1, (2 + term(i + 1, iters))) if i < iters else 0
-
-    return 1 + term(0, iters)
-
-
+numer, denom = 3, 2
 count = 0
-
 for n in range(1, 1000 + 1):
-    frac = expansion(n)
-    if len(str(frac.numerator)) > len(str(frac.denominator)):
+    if len(str(numer)) > len(str(denom)):
         count += 1
+    denom, numer = numer + denom, 2 * denom + numer
 
 print(count)
